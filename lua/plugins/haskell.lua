eec66abe20a58d205a26e360c8eab852af5ddbd0
Helix <hissyu@hotmail.com>
2024-11-18 17:36:59 +0800

    fix surround

--- lua/plugins/haskell.lua
+++ lua/plugins/haskell.lua
@@ -4,15 +4,6 @@ local ht = require("haskell-tools")
 -- haskell-language-server relies heavily on codeLenses,
 -- so auto-refresh (see advanced configuration) is enabled by default
 vim.keymap.set("n", "<space>hl", vim.lsp.codelens.run, { desc = "Run code lenses", noremap = true, silent = true })
--- Hoogle search for the type signature of the definition under the cursor
--- vim.keymap.set(
---   "n",
---   "<space>hs",
---   ht.hoogle.hoogle_signature,
---   { desc = "Hoogle signature", noremap = true, silent = true }
--- )
--- Evaluate all code snippets
--- vim.keymap.set("n", "<space>ha", ht.lsp.buf_eval_all, { desc = "Evaluate all", noremap = true, silent = true })
 
 return {
   "mrcjkb/haskell-tools.nvim",
