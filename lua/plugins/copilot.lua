f778ae1232fb42d76952fd5d0691d5c8e23ddd7b
Helix <hissyu@hotmail.com>
2024-11-21 22:16:24 +0800

    fix surround

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,23 @@
+-- local cp = require("copilot.suggestion")
+-- vim.keymap.set("i", "<C-l>", function()
+--   require("copilot.suggestion").accept_word()
+-- end, { desc = "accept_word", expr = true })
+
+return {
+  {
+    "zbirenbaum/copilot-cmp",
+    enabled = false,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept_word = "<C-l>",
+        },
+      },
+    },
+  },
+}
--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,11 +1,11 @@
--- vim.keymap.set({ "n", "v" }, "s", "<nop>")
+vim.keymap.set({ "n", "v" }, "s", "<nop>")
 
 return {
   "echasnovski/mini.surround",
   opts = {
     mappings = {
       add = "ssa", -- Add surrounding in Normal and Visual modes
-      delese = "ssd", -- Delete surrounding
+      delele = "ssd", -- Delete surrounding
       find = "ssf", -- Find surrounding (to the right)
       find_left = "ssF", -- Find surrounding (to the left)
       highlight = "ssh", -- Highlight surrounding
