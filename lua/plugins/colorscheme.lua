92baa804e092272fad9e424fc3354d29c9b15463
Helix <hissyu@hotmail.com>
2024-11-28 22:35:35 +0800

    opt some color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,14 +9,12 @@ return {
     "Shatur/neovim-ayu",
     name = "ayu",
 
-    -- opts = function()
-    --   require("nvim-treesitter.configs").setup({
-    --     rainbow = {
-    --       enable = true,
-    --       colors = require("ayu.colors").generate(),
-    --     },
-    --   })
-    -- end,
+    opts = {
+      overrides = {
+        Search = { bg = "None", fg = "#eed9ff" },
+        WildMenu = { fg = "#ffffff" },
+      },
+    },
     priority = 1000,
   },
   {
