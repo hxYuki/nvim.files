dde9c777e30a15dde5fb947b6b8311853c11567b
Helix <hissyu@hotmail.com>
2024-11-28 16:34:36 +0800

    new color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,20 @@
+vim.cmd([[
+set termguicolors
+let g:ayucolor="dark"
+]])
+
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
+    "Luxed/ayu-vim",
+    name = "ayu",
+
+    priority = 1000,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "ayu",
     },
   },
 }
