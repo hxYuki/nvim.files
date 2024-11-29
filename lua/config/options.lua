1181925688e67f59dc6d1c0e2df65a82ae0a42ff
Helix <hissyu@hotmail.com>
2024-11-29 14:31:15 +0800

    auto check platform

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,7 @@
 -- Add any additional options here
 
 -- Set default intergrated shell
-vim.o.shell = '"c:\\program files\\powershell\\7\\pwsh.exe"'
+
+if vim.fn.has("win32") == 1 then
+  vim.o.shell = '"c:\\program files\\powershell\\7\\pwsh.exe"'
+end
