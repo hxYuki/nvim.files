fda08e01eb6e08bf1dcb983d4b24e01b86bd4494
Helix <hissyu@hotmail.com>
2025-01-01 12:21:11 +0800

    add ignore for fzf-lua

--- lua/plugins/unity.lua
+++ lua/plugins/unity.lua
@@ -1,21 +1,31 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        hide_by_pattern = {
-          "*.meta",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_by_pattern = {
+            "*.meta",
+          },
         },
       },
     },
+    -- config = function ()
+    --     require("neo-tree").setup{
+    --           filesystem = {
+    --               filtered_items = {
+    --                   hide_by_pattern
+    --               }
+    --           }
+    --       }
+    -- end
   },
-  -- config = function ()
-  --     require("neo-tree").setup{
-  --           filesystem = {
-  --               filtered_items = {
-  --                   hide_by_pattern
-  --               }
-  --           }
-  --       }
-  -- end
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      files = {
+        rg_opts = [[--color=never --files --hidden --follow -g "!.git" -g "!**/*.meta" -g "!**/*.asset" -g "!**/*.prefab"]],
+      },
+    },
+  },
 }
