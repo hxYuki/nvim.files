1bded4a7dfc5f343ed1a79c0390f0dcf6c690b39
Helix <hissyu@hotmail.com>
2025-08-26 22:35:49 +0800

    add wrap line

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,5 +5,12 @@
 -- Set default intergrated shell
 
 if vim.fn.has("win32") == 1 then
-  vim.o.shell = '"c:\\program files\\powershell\\7\\pwsh.exe"'
+  vim.o.shell = '"C:\\Program Files\\WindowsApps\\Microsoft.PowerShell_7.5.2.0_x64__8wekyb3d8bbwe\\pwsh.exe"'
 end
+
+-- Set wrap line
+vim.o.wrap = true
+vim.o.showbreak = "↪"
+vim.o.linebreak = true
+vim.o.breakindent = true
+vim.o.breakindentopt = "shift:4,sbr"
