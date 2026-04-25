9f92726a86e1dcd150bbb0f04d499a4b5741dc71
Helix <hissyu@hotmail.com>
2026-04-25 11:50:50 +0800

    add bsn-fmt

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,24 +1,40 @@
 return {
-  "mrcjkb/rustaceanvim",
-  ft = { "rust" },
-  opts = {
-    server = {
-      default_settings = {
-        ["rust-analyzer"] = {
-          checkOnSave = {
-            overrideCommand = {
-              "cargo",
-              "clippy",
-              "--workspace",
-              "--message-format=json",
-              "--all-targets",
+  {
+    "mrcjkb/rustaceanvim",
+    ft = { "rust" },
+    opts = {
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            checkOnSave = {
+              overrideCommand = {
+                "cargo",
+                "clippy",
+                "--workspace",
+                "--message-format=json",
+                "--all-targets",
+              },
+            },
+            completion = {
+              callable = {
+                snippets = "none",
+              },
             },
           },
-          completion = {
-            callable = {
-              snippets = "none",
-            },
-          },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt", "bsn_fmt" },
+      },
+      formatters = {
+        bsn_fmt = {
+          command = "bsnfmt",
+          stdin = true,
         },
       },
     },
